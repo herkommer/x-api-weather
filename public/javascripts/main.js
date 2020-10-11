f4615ddc275dedd48f9b27f6a9f6cba564a36578
herkommer <m@herkommer.co.uk>
2020-10-11 15:27:28 +0200

    Added fetch AJAX OpenWeather call for Stockholm and showing result in the Console

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,4 +2,15 @@ document.querySelector("form").addEventListener("submit", e => {
   e.preventDefault();
   let inputCityValue = document.getElementById("txtCity").value;
   console.log('You selected: ' + inputCityValue);
+
+  fetch("https://api.openweathermap.org/data/2.5/weather?q=Stockholm&appid=0ebe027a1c5f50d51704718857a7c51c&units=metric")
+   .then(response => response.json())
+   .then(data => {
+      const { main, name, sys, weather } = data;
+      console.log("temperature returned: " + main.temp)
+    })
+   .catch(() => {
+    console.log("something went wrong, sorry");
+  });
+
 });
