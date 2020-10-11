b02a1eeac4021ea15ca70d714feb5ee1297a0246
herkommer <m@herkommer.co.uk>
2020-10-11 17:36:26 +0200

    Added input city as a variable in the fetch call, and removed unnecessary console.log calls

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,24 +1,11 @@
 document.querySelector("form").addEventListener("submit", e => {
   e.preventDefault();
   let inputCityValue = document.getElementById("txtCity").value;
-  console.log('You selected: ' + inputCityValue);
 
-  fetch("https://api.openweathermap.org/data/2.5/weather?q=Stockholm&appid=0ebe027a1c5f50d51704718857a7c51c&units=metric")
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputCityValue}&appid=0ebe027a1c5f50d51704718857a7c51c&units=metric`)
    .then(response => response.json())
    .then(data => {
       const { main, name, sys, weather } = data;
-      console.log("temperature returned: " + main.temp)
-
-      console.log("---Main---");
-      console.log(main);
-      console.log("---name---");
-      console.log(name);
-      console.log("---sys---");
-      console.log(sys);
-      console.log("---weather---");
-      console.log(weather);
-      console.log("===data - full object===");
-      console.log(data);
 
       const li = document.createElement("li");
       li.classList.add("city");
