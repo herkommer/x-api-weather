f848bec9a840c51a6801e5af07ed24af6317be37
herkommer <m@herkommer.co.uk>
2020-10-11 17:15:00 +0200

    Added code to generate html elements, updated template and CSS. Added LOG info for browser Console.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -8,6 +8,33 @@ document.querySelector("form").addEventListener("submit", e => {
    .then(data => {
       const { main, name, sys, weather } = data;
       console.log("temperature returned: " + main.temp)
+
+      console.log("---Main---");
+      console.log(main);
+      console.log("---name---");
+      console.log(name);
+      console.log("---sys---");
+      console.log(sys);
+      console.log("---weather---");
+      console.log(weather);
+      console.log("===data - full object===");
+      console.log(data);
+
+      const li = document.createElement("li");
+      li.classList.add("city");
+
+      const markup = `
+        <div>
+            <h2>
+                <span>${name}</span>
+                <sup>${sys.country}</sup>
+            </h2>
+            <div>${main.temp}<sup>°C</sup></div>
+        </div>
+      `;
+
+      li.innerHTML = markup;
+      document.getElementById("cities").appendChild(li);
     })
    .catch(() => {
     console.log("something went wrong, sorry");
